ec80f84e44dbff0332c01fd640b474366e809254
mandeepb29 <mandeepb29@gmail.com>
2020-03-10 15:25:32 +0530

    responsive changes

--- src/app/blog-website/blog/blog.component.ts
+++ src/app/blog-website/blog/blog.component.ts
@@ -244,27 +244,7 @@ export class BlogComponent implements OnInit {
   }
 
 
-  mayorBlogs;
-  croBlogs = {
-    margin: 25,
-    nav: true,
-    dots: false,
-    stagePadding: 30,
-    navText: ['<img src="assets/img/icons/prev.svg" style="width:30px;">', '<img src="assets/img/icons/next.svg" style="width:30px;">'],
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-        stagePadding: 50
-      },
-     1000: {
-        items: 3,
-      }
-    }
-  }
+ 
 
   //owl carousel settings for featured blog cards
   carouselOptions = {
