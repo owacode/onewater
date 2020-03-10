bc2944d109d51c79e455622bb2135c9ce9df65cb
mohitpatell <mohit@wednesday.is>
2020-03-11 01:39:50 +0530

    event link done - mohit

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from './header.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [HeaderComponent]
 })
