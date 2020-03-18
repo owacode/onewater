ed4fa2446e6b446ce89780e4d414f9bc741ebe43
mandeepb29 <mandeepb29@gmail.com>
2020-03-18 22:19:10 +0530

    safe html pipe added - mandeep

--- src/app/pipes/common-pipes.module.ts
+++ src/app/pipes/common-pipes.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShortenPipe } from './shorten.pipe';
+import { SafeHtmlPipe } from './safeHtml.pipe';
 
 @NgModule({
-  declarations: [ShortenPipe],
+  declarations: [ShortenPipe,SafeHtmlPipe],
   imports: [
     CommonModule
   ],
   exports: [
-    ShortenPipe
+    ShortenPipe,
+    SafeHtmlPipe
   ]
 })
 
