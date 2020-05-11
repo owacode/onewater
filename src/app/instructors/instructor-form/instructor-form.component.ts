170196c08c408852efca1378e6f2dd6c6dd3a2f8
mandeepb29 <mandeepb29@gmail.com>
2020-05-11 19:02:50 +0530

    update mandeep

--- src/app/instructors/instructor-form/instructor-form.component.ts
+++ src/app/instructors/instructor-form/instructor-form.component.ts
@@ -24,7 +24,7 @@ export class InstructorFormComponent implements OnInit {
       email: new FormControl(null, {validators:[Validators.required,Validators.email]}),
       organization: new FormControl(null, {validators:[Validators.required]}),
       mobile_no: new FormControl(null),
-      proposed_course_topic: new FormControl(null),
+      //proposed_course_topic: new FormControl(null),
       category: new FormControl('',{validators:[Validators.required]}),
       course_type: new FormControl(null, {validators:[Validators.required]}),
       proposed_course_title: new FormControl(null, {validators:[Validators.required]}),
